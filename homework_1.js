981268fde3c1cbaa2974de6d13db163e85bbf9de
Lubov Muraveva <lyubchikm@yandex.ru>
2024-10-06 21:18:29 +0300

    первый комит

---
+++ homework_1.js
@@ -0,0 +1,73 @@
+console.log("Hello, world!");
+
+let int = 5; //number
+
+//console.log(int);
+
+//console.log(int,typeof int);
+
+console.log(int,typeof (int));
+
+let str = 'JavaScript'; //string
+
+//console.log(str);
+
+console.log(str,typeof (str));
+
+let direction = null; //null
+
+//console.log(direction);
+
+console.log(direction,typeof (direction));
+
+let isValue = true; //boolean
+
+//console.log(isValue);
+
+console.log(isValue,typeof (isValue));
+
+let num; // undefined
+
+//console.log(num);
+
+console.log(num,typeof (num));
+
+const bigint = 8950422584340004n; //bigInt
+
+//console.log(bigint);
+
+//const bigint = 1111111111n; //SyntaxError: Identifier 'bigint' has already been declared
+
+console.log(bigint,typeof (bigint));
+
+const sym = Symbol('id'); //symbol
+
+//console.log(sym);
+
+console.log(sym,typeof (sym));
+
+const person = { name: 'Люба'} //object
+
+//console.log(person);
+
+person.name = 'Оля'; //{ name: 'Оля' } object
+
+console.log(person,typeof (person));
+
+let obj = { a: 5 } //object
+
+//console.log(obj);
+
+obj = { a: 10 } // { a: 10 } object
+
+console.log(obj,typeof (obj));
+
+var blackDog = { color: 'black' } //object
+
+//console.log(blackDog);
+
+blackDog = { color: 'grey' }; //{ color: 'grey' } object
+
+console.log(blackDog,typeof (blackDog));
+
+//add Git
